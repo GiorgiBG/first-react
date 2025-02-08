97a250b45ff51c31a161f2e4843c1b2ddc1be3dc
giorgibgujito@gmail.com <giorgibgujito@gmail.com>
2025-02-08 14:21:35 +0200

    Last commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar (){
+    return (
+           <header>
+                <nav>
+                    <img src="./src/assets/react-logo.png" alt="React logo" />
+                    <p>ReactFacts</p>
+                </nav>
+           </header>
+
+        )
+}
